80865a2675b172ea421bbb01bb06ac2515880448
khairulhabibie <khairulhabibie01@gmail.com>
2023-08-21 15:50:48 +0700

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,45 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function NavbarComp() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand>Khairul's Web</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/project">Project</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComp;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <section>
+      <h1 className="mt-5">HI !</h1>
+      <h1>I'am Khairul</h1>
+      <p>
+        I work with React Ecosystem, and write to remember how to rebuild and
+        redefine fundamental concepts through mental models.
+      </p>
+    </section>
+  );
+}
+
+export default AboutPage;
